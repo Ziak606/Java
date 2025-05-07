d4ad065143226e845fe6189d5930ca9c7d7130a8
Ziak606 <ziak606@gmail.com>
2025-05-07 12:24:02 +0530

    array

---
+++ arrays/ArrLists.java
@@ -0,0 +1,18 @@
+package arrays;
+import java.util.ArrayList;
+public class ArrLists {
+
+    public static void main (String [] args){
+
+        ArrayList<String> str = new ArrayList<String>();
+        str.add("Geeks");
+        str.add("for");
+        str.add("Geeks");
+
+        System.out.println(str.get(0));
+
+
+
+
+    }
+}
